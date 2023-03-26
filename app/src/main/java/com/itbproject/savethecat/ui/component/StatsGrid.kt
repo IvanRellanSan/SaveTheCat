f3df119e5af0be3237d9054c86c12ebe085dca3c
Iván Rellán <ivan.rellan.7e5@itb.cat>
2023-03-26 23:40:30 +0200

    Added components for detail

---
+++ app/src/main/java/com/itbproject/savethecat/ui/component/StatsGrid.kt
@@ -0,0 +1,17 @@
+package com.itbproject.savethecat.ui.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.itbproject.savethecat.model.BreedModel
+
+@Composable
+fun StatsGrid(breedModel: BreedModel, modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun StatsGridPreview() {
+
+}
